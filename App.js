600e993305ccb5044cc5aa3df07e79db4211629b
Audrey Costa <audrey.costa@netconamericas.com>
2023-07-20 12:07:02 -0300

    Build a screen

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
+import PlaceholderImage from './assets/images/background-image.png'
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer PlaceholderImageSource={ PlaceholderImage }/>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme="primary"/>
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58
+  },
+  footerContainer: {
+    flex: 1/3,
+    alignItems: 'center',
   },
 });
